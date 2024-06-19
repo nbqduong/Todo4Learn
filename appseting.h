df0a06eb372ab867a64d7393769264d71855db0a
nbqduong <nbqduong@gmail.com>
2024-06-20 06:59:11 +0700

    add a part of setting

--- appseting.h
+++ appseting.h
@@ -2,14 +2,27 @@
 #define APPSETING_H
 
 #include <QObject>
+#include <QDebug>
+#include <QSettings>
+#include <QCoreApplication>
+#include <QVariant>
+#include <QMap>
+#include <QMapIterator>
 
 class appSeting : public QObject
 {
     Q_OBJECT
+
+    QSettings appAttribute;
+
 public:
-    explicit appSeting(QObject *parent = nullptr);
+    explicit appSeting(QString organizationName, QString appName );
+    void saveSettings(QString group, QMap<QString, QVariant> map);
+    int getSettings(QString group, QString name);
 
 signals:
 };
+
+
 
 #endif // APPSETING_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <appseting.h>
+
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    appSeting a("a","b");
+    QMap<QString, QVariant> map;
+    map.insert("plenty", 100);
+    map.insert("plenty1", 1100);
+    a.saveSettings("a", map);
+    qInfo() << a.getSettings("a","plenty");
+
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/todoApp/Main.qml"));
